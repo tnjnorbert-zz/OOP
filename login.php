d7fea46dae5fda66424756e61dfb4c0ad8dcff21
Norbert Tumukunde <tnjnorbert@gmail.com>
2015-01-12 17:34:42 +0300

    crtesting

--- login.php
+++ login.php
@@ -5,5 +5,5 @@
  * Date: 1/9/15
  * Time: 9:04 AM
  */
-mysql_connect("http://192.168.1.6","root","rickross");
+mysql_connect("http://1192.168.1.255","root","rickross");
 mysql_select_db("profile_v2");
